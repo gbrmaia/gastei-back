e70d4c35808319af1b88e640bc40c7a280ef20fe
Gabriel Maia <gabrielmaia43@hotmail.com>
2024-07-11 16:30:49 -0300

    add cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Configurar CORS
+  app.enableCors({
+    origin: 'http://localhost:3000', // URL do seu frontend
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
   await app.listen(4000);
 }
 bootstrap();
